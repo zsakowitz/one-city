68ba3283485031e7c30768c028bfaf7de9ab9d95
MrTinker64 <avi.micah@gmail.com>
2023-03-20 18:29:30 -0700

    Added some more stuff off the oficial site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,15 @@
 <article>
-  <h1>OneCity Website</h1>
-  <p>Look, it's some text!</p>
+  <h1>One City | Sharing Our Abundance</h1>
+  <p style="text-align:center;white-space:pre-wrap;" class=""><strong>'אַחֲרֵי־כֵ֗ן יִקָּ֤רֵא לָךְ֙ עִ֣יר הַצֶּ֔דֶק<br><br>’After that, you shall be called a City of Righteousness...'<br>-Isaiah1:26</strong></p>
 </article>
+
+<style>
+    h1 {
+        color: black;
+        margin: 2rem 0;
+        font-size: calc((2.8 - 1) * calc(.012 * min(100vh, 900px)) + 1rem);
+        line-height: calc(1.3 * (1 + (1 - 2.8)/25));
+        font-family: ivymode;
+        font-weight: 600;
+    }
+</style>
