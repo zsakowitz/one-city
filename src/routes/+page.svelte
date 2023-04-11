357c999af70052e860c594868dfce78ebab86803
Zachary Sakowitz <zsakowitz@gmail.com>
2023-04-10 17:19:56 -0700

    Fix homepage styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,10 @@
-<article>
-  <div class="sqs-block-content">
-    <h1 class="dark:text-slate-100" style="text-align:center;white-space:pre-wrap;">One City | Sharing Our Abundance</h1>
-    <p class="dark:text-slate-300" style="text-align:center;white-space:pre-wrap;">
-      <strong>אַחֲרֵי־כֵ֗ן יִקָּ֤רֵא לָךְ֙ עִ֣יר הַצֶּ֔דֶק<br>
-        <br>'After that, you shall be called a City of Righteousness...'
-        <br>-Isaiah1:26</strong>
-    </p>
-  </div> 
-</article>
+<div>
+  <h1 class="my-8 text-center text-4xl font-semibold text-z-heading">
+    One City | Sharing Our Abundance
+  </h1>
 
-<style>
-  h1 {
-    color: black;
-    margin: 2rem 0;
-    /* font-size: calc((2.8 - 1) * calc(.012 * min(100vh, 900px)) + 1rem); */
-    font-size: 32.9344px;
-    line-height: -0.0416;
-    font-family: ivymode;
-    font-weight: 600;
-  }
-  p {
-    color: black;
-  }
-</style>
+  <p class="text-center">
+    'אַחֲרֵי־כֵ֗ן יִקָּ֤רֵא לָךְ֙ עִ֣יר הַצֶּ֔דֶק<br /><br />“After that, you
+    shall be called a City of Righteousness...”<br /> – Isaiah 1:26
+  </p>
+</div>
