69368c109adb2d6d5ad77fafc432f1a051779917
Zachary Sakowitz <zsakowitz@gmail.com>
2023-05-08 18:09:52 -0700

    Make log in work

--- src/routes/log-in/+page.server.ts
+++ src/routes/log-in/+page.server.ts
@@ -1,5 +1,7 @@
 import { Account } from "$lib/server/account"
 import { getFormData } from "$lib/server/get-form"
+import { unwrapOr500 } from "$lib/server/unwrap.js"
+import { redirect } from "@sveltejs/kit"
 
 export const actions = {
   async default(event) {
@@ -7,6 +9,16 @@ export const actions = {
 
     const account = new Account({ email })
 
-    return { ok: true }
+    const { currentSession } = unwrapOr500(
+      await account.select({ currentSession: true })
+    )
+
+    event.cookies.set("1city_current_session", currentSession, {
+      path: "/",
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+      maxAge: 60 * 60 * 24 * 30,
+    })
+
+    throw redirect(303, "/")
   },
 }
