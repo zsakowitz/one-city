37380d082c7874a92bbb0c8095c964dd7aec7976
Zachary <zsakowitz@gmail.com>
2023-12-21 14:04:25 -0800

    Change homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,41 +25,30 @@
 
 <div class="mx-auto w-[32rem] max-w-full">
   <h2 class="my-4 text-left text-2xl font-semibold">What is One City?</h2>
+
   <p class="my-2">
     One City is a project to connect the GLIDE and Kitchen communities. In
-    partnership with the <a
-      class="underline"
-      href="https://www.glide.org/program/walk-in-center/"
-      target="_blank">GLIDE Walk-In-Center</a
-    >, we are supporting GLIDE clients furnish their homes.
+    partnership with the <a href="https://www.glide.org/program/walk-in-center/"
+      >GLIDE Walk-In-Center</a
+    >, we are supporting GLIDE clients to furnish their homes.
   </p>
+
   <p class="my-2">
-    The walk-in center works with hundreds of bright, righteous people each week
+    The Walk-In-Center works with hundreds of bright, righteous people each week
     who are wrestling with poverty, addiction and/or mental health challenges.
-    Many of these people are trying to meet their basic needs—shelter,
-    furniture, clothing.
+    Many of these people are trying to meet their basic needs, such as shelter,
+    furniture, or clothing.
   </p>
+
   <p class="my-2">
-    Our monthly Team Captains &amp; GLIDE staff have created a database of
-    needed items. If you have any of the items and are ready to donate, email us <a
-      class="underline"
+    Our monthly Team Captains & GLIDE staff have created a database of needed
+    items. If you have any of the items and are ready to donate, email us <a
       href="mailto:onecity@thekitchensf.org">here</a
-    > and monthly team captain will reach out with next steps.
+    >
+    and monthly a team captain will reach out with next steps.
   </p>
+
   <p class="my-2">
-    <em
-      >One City is orchestrated by a group of loving volunteers from The Kitchen
-      and Avital Raff, The Kitchen’s Community Organizer.
-    </em>
+    Thank you for helping to make a difference in the lives of others.
   </p>
 </div>
-
-<!-- <div class="justify-center mt-6 flex gap-4">
-  <a class="field" href="/log-in">Log In</a>
-  <a class="field" href="/sign-up">Sign Up</a>
-</div>
-
-
-{#if data.name}
-  <p class="text-center mt-6 text-3xl font-light">Welcome, {data.name}!</p>
-{/if} -->
